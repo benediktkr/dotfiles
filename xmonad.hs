188c3352dea99036f4c222754a0248c2653af293
Benedikt Kristinsson <benedikt.k@gmail.com>
2011-06-06 15:23:01 +0000

    * xmobar added to XMonad config * focusFollowMouse = False * additionalKeys: scrot, lock screen

--- xmonad.hs
+++ xmonad.hs
@@ -3,9 +3,36 @@
 -- Benedikt Kristinsson
 -- Welcome down the rabbit hole
 --
+-- Requires (ubuntu packages):
+--   dmenu
+--   libghc6-xmonad-contrib-dev
+--   libghc6-xmonad-dev
+--   xmobar
 
 import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad $ defaultConfig {
-  terminal = "xterm"
-  }
+main = do
+  -- xmobar accepts input on its stdin. Rarther then
+  --   running `monad | xmobar`, we do this.
+  xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+  xmonad $ defaultConfig {
+    modMask = mod4Mask,
+    terminal = "xterm",
+    focusFollowsMouse = False,
+    manageHook = manageDocks <+> manageHook defaultConfig,
+    layoutHook = avoidStruts  $  layoutHook defaultConfig,
+    logHook = dynamicLogWithPP xmobarPP {
+      -- We get output to xmobar with hPutStrLn xmproc (a pipe to xmobar)
+      ppOutput = hPutStrLn xmproc,
+      ppTitle = xmobarColor "green" "" . shorten 50
+      }
+    } `additionalKeys`
+    [ ((0, xK_Print), spawn "scrot ~/scrot.png"),
+      ((mod4Mask, xK_z), spawn "xscreensaver-command -lock")
+    ]
+
