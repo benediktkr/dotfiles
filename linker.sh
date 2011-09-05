d30390e5c407e70cef3b7d2a057900c90fb5b4a3
Benedikt Kristinsson <benedikt.k@gmail.com>
2011-09-05 12:56:04 +0000

    Linker, split xmobarrc up for each host, added host-specifics to xsession.

---
+++ linker.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Linker for my dotfiles
+
+if [ `hostname` = 'benedikt-laptop' ]; then
+    ln -s $PWD/bashrc ~/.bashrc
+fi
+
+ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
+ln -s $PWD/xmobarrc ~/.xmobarrc
+
+ln -s $PWD/screenrc ~/.screenrc
+
+ln -s $PWD/Xresources ~/.Xresources
+ln -s $PWD/xsession ~/.xsession
+
+# Make alt act like meta. Originally for irssi in xterm
+ln -s $PWD/inputrc ~/.inputrc
+
