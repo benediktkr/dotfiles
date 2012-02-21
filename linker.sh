0e8c86b733ee993ca23bce86ae7db2d3f86ef6b0
Benedikt Kristinsson <benedikt.k@gmail.com>
2012-02-21 20:19:43 +0000

    New gfx vard in turing

--- linker.sh
+++ linker.sh
@@ -2,19 +2,32 @@
 
 # Linker for my dotfiles
 
+# Only laptop
 if [ `hostname` = 'benedikt-laptop' ]; then
     ln -s $PWD/bashrc ~/.bashrc
 fi
 
+
+# Only desktop
+if  [ `hostname` = 'turing' ]; then 
+    echo "turing: xorg.conf"
+    if [ -f "/etc/X11/xorg.conf" ]; then
+        sudo mv /etc/X11/xorg.conf ~/xorg.conf-old
+        echo "Note: Old xorg.conf backed up to ~/xorg.conf-old"
+    fi
+    sudo ln -s $PWD/xorg.conf-turing /etc/X11/xorg.conf
+fi
+
+
 # Computers with X (assume XMonad)
 if [ `hostname` = 'benedikt-laptop' ] || [ `hostname` = 'turing' ]; then
-    echo "X and XMonad assumed"
     ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
     ln -s $PWD/xmobarrc-`hostname` ~/.xmobarrc
 
     ln -s $PWD/Xresources ~/.Xresources
     ln -s $PWD/xsession ~/.xsession
 fi
+
 
 ln -s $PWD/screenrc ~/.screenrc
 
