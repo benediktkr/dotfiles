8e57bb35d3fac949c9e97946dc7215e7ca02388b
Benedikt Kristinsson <benedikt.k@gmail.com>
2012-03-04 21:55:11 +0000

    Interactive delete in linker

--- linker.sh
+++ linker.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-read -p 'Will overwrite existing config files. Continue? (y/n) '
-[ $REPLY != "y" ] && exit 1
-
 # Linker for my dotfiles
-ln -s $PWD/bashrc ~/.bashrc
+# Checks if a file exists and does interactive rm. If user says no
+# the script will still try to link, but failes because the file
+# already exists. 
+
+#read -p 'Will overwrite existing config files. Continue? (y/n) '
+#[ $REPLY != "y" ] && exit 1
+
+
+rm -i ~/.bashrc && ln -s $PWD/bashrc ~/.bashrc
 
 # Only desktop
 if  [ `hostname` = 'turding' ]; then 
@@ -19,23 +24,36 @@ fi
 
 if hash X 2>&-; then
     echo "Found X..."
+    [ -f '~/.Xresources' ] && rm -i ~/.Xresources
     ln -s $PWD/Xresources ~/.Xresources
+
+    [ -f '~/.xsession' ] && rm -i ~/.xsession
     ln -s $PWD/xsession ~/.xsession
 fi
 
 if hash xmonad 2>&-; then
     echo "Found XMonad..."
+    if [ -d '~/.xmonad/' ] then
+        [ -f '~/.xmonad/xmonad.hs' ] && rm -i .xmonad/xmonad.hs
+    else
+        mkdir ~/.xmonad/
+    fi
+    
     ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
     if [ -f $PWD/xmobarrc-`hostname` ]; then
         ln -s $PWD/xmobarrc-`hostname` ~/.xmobarrc
+    else
+        # Assume the turing config as the default
+        ln -s $PWD/xmonarrc-turing ~/.xmobarrc
     fi
 fi
 
 
 if hash screen 2>&-; then
     echo "Found GNU Screen..."
-    mv ~/.screenrc /tmp
+    [ -f '~/.screenrc' ] && rm -i screenrc
     ln -s $PWD/screenrc ~/.screenrc
-    # Makes alt act like meta. Originally for irssi in xterm.. I think. 
+    # Makes alt act like meta. Originally for irssi in xterm.. I think.
+    [ -f '~/.inputrc' ] && rm -i .inputrc
     ln -s $PWD/inputrc ~/.inputrc
 fi
