a8090dc7a468c09f392f22f4023b5a5e2590804e
Benedikt Kristinsson <benedikt.k@gmail.com>
2012-03-04 19:45:48 +0000

    Ask

--- linker.sh
+++ linker.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-# Linker for my dotfiles
+read -p 'Will overwrite existing config files. Continue? (y/n)'
+[ $REPLY != [yY] ] && exit 1
 
+# Linker for my dotfiles
 ln -s $PWD/bashrc ~/.bashrc
 
 # Only desktop
@@ -24,7 +26,7 @@ fi
 if hash xmonad 2>&-; then
     echo "Found XMonad..."
     ln -s $PWD/xmonad.hs ~/.xmonad/xmonad.hs
-    if [ -f $PWD/xmobarrc-`hostname` ]; then 
+    if [ -f $PWD/xmobarrc-`hostname` ]; then
         ln -s $PWD/xmobarrc-`hostname` ~/.xmobarrc
     fi
 fi
@@ -32,6 +34,7 @@ fi
 
 if hash screen 2>&-; then
     echo "Found GNU Screen..."
+    mv ~/.screenrc /tmp
     ln -s $PWD/screenrc ~/.screenrc
     # Makes alt act like meta. Originally for irssi in xterm.. I think. 
     ln -s $PWD/inputrc ~/.inputrc
