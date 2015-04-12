7c725deeb6282040c6bd51cbd9177bd3e3b26be7
Benedikt Kristinsson <benedikt@inventati.org>
2015-04-12 16:28:18 +0000

    moving xmonad stuff aroudn

---
+++ start-xmonad.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Load resources and Xmodmap (assumes that files have been linked)
+#xrdb -merge ~/.Xresources
+#xmodmap ~/.Xmodmap
+
+BASE_DIR=/home/benedikt/projects/dotfiles
+
+# Put trayer in the left corner
+#trayer --edge top --align right --SetDockType true --SetPartialStrut true  \
+#  --expand true --width 2 --transparent true --tint 0x191970 --height
+
+# Set the background. 
+WALLPAPER=chamberofunderstanding.jpg
+if hash feh 2>&- && [ -f "$BASE_DIR/bgpics/$WALLPAPER" ]; then
+    feh --bg-fill $HITHER/$WALLPAPER --no-fehbg
+fi
+
+if hash xscreensaver 2>&-; then 
+    xscreensaver -no-splash &
+fi
+
+if hash nm-applet 2>&-; then
+    nm-applet &
+fi
+
+# Host-dependent settings
+if [ `hostname` = 'burrow' ]; then
+      blank_time=5
+fi
+
+xset b off
+# I sometimes turn DPMS off and forget to turn it back on. 
+xset +dpms  
+xset s $[$blank_time*60]
+
+eval `ssh-agent -s`
+
+exec xmonad
