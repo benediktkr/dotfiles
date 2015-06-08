a4cac686a3522e33efe68f6e1a8ac75964bba113
Benedikt Kristinsson <benedikt@inventati.org>
2015-06-08 22:35:47 +0000

    backup borrow

--- duplicity/backup-burrow.sh
+++ duplicity/backup-burrow.sh
@@ -1,41 +1,30 @@
 set -e
-
 export PASSPHRASE=""
 KEYID="0x61FD1056812214D6"
-repDate=`date +%Y%m%d`;
-
-echo "Performing backup.."
 
 /usr/bin/duplicity \
+    --verbosity 4 \
+    --full-if-older-than 7D \
     --include /home/benedikt/documents \
     --include /home/benedikt/projects \
     --include /home/benedikt/.ssh \
+    --include /home/benedikt/scrots \
+    --include /home/benedikt/.gnupg \
     --exclude '**' \
     --volsize 1000 \
-    /home/benedikt/ scp://duplicity@freespace.sudo.is/burrow/ \
+    --max-blocksize 16777216 \
+    --gpg-options "--bzip2-compress-level=0" \
+    /home/benedikt/ sftp://duplicity@freespace.sudo.is/burrow/ \
     --ssh-options="-oIdentityFile=/home/benedikt/.ssh/backupkey" \
     --encrypt-key $KEYID \
-    --sign-key $KEYID # \
-#    > /home/benedikt/.duplicity/burrow_${repDate}.log
+    --sign-key $KEYID
 
-echo "Getting list of backups.."
+/usr/bin/duplicity collection-status sftp://duplicity@freespace.sudo.is/burrow/ \
+    --ssh-options="-oIdentityFile=/home/benedikt/.ssh/backupkey"
 
-/usr/bin/duplicity \
-    collection-status \
-    /home/benedikt/ scp://duplicity@freespace.sudo.is/burrow/ \
-    --ssh-options="-oIdentityFile=/home/benedikt/.ssh/backupkey" \
-    >> /home/benedikt/.duplicity/burrow_${repDate}.log
-
-# Mail me the results
-#mail -t localuser -s "localmachine Backup Incremental (Admin)" < /home/localuser/Documents/BackupLogs/Incremental_Backup_Log_${repDate}_localmachine_admin.log
-
-
-# Do cleanup after the backup
-/usr/bin/duplicity \
-    remove-older-than 14D \
-    -v9 \
+/usr/bin/duplicity remove-older-than "8D" \
     --force \
-    /home/benedikt/ scp://duplicity@freespace.sudo.is/burrow/ \
-    --ssh-options="-oIdentityFile=/home/benedikt/.ssh/backupkey" \
+    sftp://duplicity@freespace.sudo.is/burrow/ \
+    --ssh-options="-oIdentityFile=/home/benedikt/.ssh/backupkey"
 
 unset PASSPHRASE
