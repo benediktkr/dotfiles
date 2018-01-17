ef8884d5499a749251dbb049063abbdee35998f5
Benedikt Kristinsson <benedikt.kristinsson@care.com>
2018-01-17 12:58:40 -0500

    pushing configs

---
+++ push-configs.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="push-configs.py")
+parser.add_argument(
+    "hosts",
+    type=str,
+    nargs='+',
+    help="hosts to sync to")
+parser.add_argument(
+    "--emacs",
+    action="store_true",
+    help="push emacs config")
+parser.add_argument(
+    "--zsh",
+    action="store_true",
+    help="push zsh config")
+parser.add_argument(
+    "--dry-run",
+    action="store_true",
+    help="print scp commands, do not run them")
+args = parser.parse_args()
+
+## TODO: Make a --care option to push to the relevant hosts
+
+emacs_config = {
+    "emacs/.emacs": "~/.emacs",
+    "emacs/.emacs.d/": "~/.emacs.d/",
+}
+
+zsh_config = {
+    "zsh/.zshrc": "~/.zshrc",
+}
+
+def scp_cmds(host):
+    configs = {}
+    if args.emacs:
+        configs.update(emacs_config)
+    if args.zsh:
+        configs.update(zsh_config)
+
+    for local, remote in configs.items():
+        yield "scp {local} {host}:{remote}".format(**locals())
+
+if __name__ == "__main__":
+    for hostname in args.hosts:
+        for a in scp_cmds(hostname):
+            if args.dry_run:
+                print a
+            else:
+                print a
+                os.system(a)
