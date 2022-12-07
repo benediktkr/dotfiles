96ac0863e71e16b45976bbb29f9a1210447f8ec7
Ben Kristinsson <ben@sudo.is>
2022-12-07 10:13:53 +0100

    ended up revamping the whole emacs config when setting up lsp

--- emacs/rust.el
+++
@@ -1,5 +0,0 @@
-
-;(setq racer-rust-src-path "<path-to-rust-srcdir>/src/")
-(setq racer-cmd "/home/benedikt/github/racer/target/release/racer")
-(add-to-list 'load-path "/home/benedikt/github/racer/editors/emacs")
-(eval-after-load "rust-mode" '(require 'racer))
