cb5cd7823bf1f0012246b8d1793fb5150296cd97
ben <ben@git.sudo.is>
2024-02-26 14:09:59 +0000

    remove weird character

--- .local/bin/mac-tilde.sh
+++ .local/bin/mac-tilde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 
-if [[ "$1"˜ != "restore" ]]; then
+if [[ "$1" != "restore" ]]; then
     hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
 else
     hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000064}]}'
