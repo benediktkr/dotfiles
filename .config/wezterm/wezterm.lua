6c466c292d6f07ee238bfe3fa41f615643c89b97
Ben Kristinsson <ben@sudo.is>
2024-04-22 01:04:25 +0200

    wezterm colors

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,12 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'nord'
+-- config.color_scheme = 'nord'
+-- config.color_scheme = 'Everforest Dark'
+-- config.color_scheme = 'Everforest Light'
+-- config.color_scheme = 'Everblush'
+-- config.color_scheme = 'Jup'
+config.color_scheme = 'Breeze'
 config.font = wezterm.font 'Monaco'
 config.font_size = 14
 
