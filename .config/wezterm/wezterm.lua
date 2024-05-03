84acf8ab78d3ef40962811db83bf0b59da83cc1f
Ben Kristinsson <benedikt.kristinsson@care.com>
2024-05-03 20:34:26 +0200

    Clean up Wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,14 +2,15 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
--- config.color_scheme = 'nord'
+config.color_scheme = 'nord'
+config.font = wezterm.font 'DejaVu Sans Mono'
+config.font_size = 14
+
 -- config.color_scheme = 'Everforest Dark'
 -- config.color_scheme = 'Everforest Light'
 -- config.color_scheme = 'Everblush'
 -- config.color_scheme = 'Jup'
-config.color_scheme = 'Breeze'
-config.font = wezterm.font 'Monaco'
-config.font_size = 14
+-- config.color_scheme = 'Breeze'
 
 return config
 
