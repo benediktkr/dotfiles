1a84a4c31d5d85946a39d9772413bbb7666fe937
Ben Kristinsson <ben@sudo.is>
2024-06-30 16:44:17 +0200

    mac-tilde: restore/reset

--- .local/bin/mac-tilde.sh
+++ .local/bin/mac-tilde.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 #
 
-if [[ "$1" != "restore" ]]; then
+usage() {
+    echo "$(basename $0) [<restore|reset>] [-h]"
+    exit 1
+}
+
+if [[ "$1" == "restore" ]]; then
+    hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000064}]}'
+elif [[ "$1" == "reset" ]]; then
+    hidutil property --set '{"UserKeyMapping":[]}'
+elif [[ -z "$1" ]]; then
     hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
 else
-    hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000064}]}'
+    usage
 fi
