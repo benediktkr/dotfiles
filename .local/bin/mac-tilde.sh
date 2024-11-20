2dc42121438f970496a7e74a8804dfcef6a5df28
Ben Kristinsson <ben@sudo.is>
2024-11-20 13:15:39 +0100

    mac-tilde.sh show

--- .local/bin/mac-tilde.sh
+++ .local/bin/mac-tilde.sh
@@ -2,7 +2,7 @@
 #
 
 usage() {
-    echo "$(basename $0) [<restore|reset>] [-h]"
+    echo "$(basename $0) [<restore|reset|show>] [-h]"
     exit 1
 }
 
@@ -10,6 +10,8 @@ if [[ "$1" == "restore" ]]; then
     hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000035},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000064}]}'
 elif [[ "$1" == "reset" ]]; then
     hidutil property --set '{"UserKeyMapping":[]}'
+elif [[ "$1" == "show" ]]; then
+    hidutil property --get UserKeyMapping
 elif [[ -z "$1" ]]; then
     hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000035,"HIDKeyboardModifierMappingDst":0x700000064},{"HIDKeyboardModifierMappingSrc":0x700000064,"HIDKeyboardModifierMappingDst":0x700000035}]}'
 else
